02ad3f0a1e17d4e7c0d8666ba03df98501fa6ef1
warano02 <carineteoi@gmail.com>
2026-07-11 10:23:56 +0100

    feat: implementing reverse proxy mode in gateway api

--- proxy/src/bot/routes.js
+++ proxy/src/bot/routes.js
@@ -1,6 +1,7 @@
-const Filter = require("./filter")
-
+require("dotenv").config()
 const router = require("express").Router()
+const axios = require("axios")
+const Filter = require("./filter")
 
 router.post("/", async (req, res) => {
     try {
@@ -13,8 +14,9 @@ router.post("/", async (req, res) => {
         if (analyse.violation) return res.status(403).json({ err: true, msg: analyse?.rationale || "this message have injection ", injection: analyse.category || "" })
 
         //Forward request to real API
+        const { data: response } = await axios.post(process.env.AI_LAB + "/agent", { prompt })
 
-        res.json({ error: false, msg: "Request tranfert to real api " })
+        res.json(response)
     } catch (e) {
         console.log("Error occured while trying to solve prompt  injection in the request  ", e)
         return res.json({ msg: "Ta maman mola !" })
